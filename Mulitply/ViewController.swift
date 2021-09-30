9f5993f2347b48317746a643a904a4dfd8289f62
Ruben rojo <61779@psdschools.org>
2021-09-30 10:57:15 -0600

    gang

--- Mulitply/ViewController.swift
+++ Mulitply/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func myButton(_ sender: Any)
+    {
+        var number1 = text1.text ?? "0"
+        let myInt2 = Int(number1) ?? 0
+        var number2 = text2.text ?? "0"
+        let myInt3 = Int(number2) ?? 0
+  
+    var product = 
+    
+    
+    }
 }
 
